4ca7f1698f66923edddb3eac6e89485f5b42c4a1
zelfronital <denisthamrin@gmail.com>
2015-05-05 20:28:42 +1000

    Init method implemented in dumb player

--- src/asutanahadi/squatter/FirstDumbPlayer.java
+++ src/asutanahadi/squatter/FirstDumbPlayer.java
@@ -1,19 +1,44 @@
 package asutanahadi.squatter;
 
 import java.io.PrintStream;
+import java.util.Random;
 
 public class FirstDumbPlayer implements Player, Piece {
-
+	Board b;
+	int playerSide;
+	
+	/* This funstion is called by the referee to initialise the player.
+	 *  Return 0 for successful initialization and -1 for failed one.
+	 */
 	@Override
 	public int init(int n, int p) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if ((p == Piece.WHITE || p == Piece.BLACK) && n > 0) {
+			playerSide = p;
+			b = new Board(n);
+			return 0;
+		} 
+		else {
+			return -1;
+		}
 	}
 
+	
 	@Override
+	/* Function called by referee to request a move by the player.
+	 *  Return object of class Move
+	 */
 	public Move makeMove() {
-		// TODO Auto-generated method stub
-		return null;
+		Move m = new Move();
+		Random rand = new Random();
+		int dimension = b.getDimension();
+		
+		m.P = this.playerSide;
+		m.Col = rand.nextInt(dimension);
+		m.Row = rand.nextInt(dimension);
+		
+		// check if there is a piece on that position.
+		return m;
 	}
 
 	@Override
