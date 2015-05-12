a85d34c16319e88d2ecf9027ffcfe63ac262d181
zelfronital <denisthamrin@gmail.com>
2015-05-12 23:36:30 +1000

    update board

--- src/asutanahadi/squatter/BoardTest.java
+++ src/asutanahadi/squatter/BoardTest.java
@@ -18,11 +18,15 @@ public class BoardTest {
 	// Simple init test
 	public void init(){
 		board = new Board(7);
-		assertEquals((int)7, (int)board.getDimension());
-		board.addPiece(0, 0, CellContent.WHITE);
-		assertEquals(CellContent.WHITE, board.getGrid()[0][0]);
-		assertEquals(CellContent.FREE, board.getGrid()[6][6]);
-		assertEquals(false, board.isFinished());
+		try {
+			assertEquals((int)7, (int)board.getDimension());
+			board.addPiece(0, 0, CellContent.WHITE);
+			assertEquals(CellContent.WHITE, board.getGrid()[0][0]);
+			assertEquals(CellContent.FREE, board.getGrid()[6][6]);
+			assertEquals(false, board.isFinished());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	
 	// Functional testing.
@@ -34,16 +38,17 @@ public class BoardTest {
 	@Test
 	public void add_loop_square(){
 		board = new Board(7);
-		board.addPiece(0, 0, CellContent.WHITE);
-		board.addPiece(0, 1, CellContent.WHITE);
-		board.addPiece(0, 2, CellContent.WHITE);
-		board.addPiece(1, 2, CellContent.WHITE);
-		board.addPiece(2, 2, CellContent.WHITE);
-		board.addPiece(2, 1, CellContent.WHITE);
-		board.addPiece(2, 0, CellContent.WHITE);
-		board.addPiece(1, 0, CellContent.WHITE);
-		board.addPiece(1, 1, CellContent.BLACK);
-		try {
+		try {			
+			board.addPiece(0, 0, CellContent.WHITE);
+			board.addPiece(0, 1, CellContent.WHITE);
+			board.addPiece(0, 2, CellContent.WHITE);
+			board.addPiece(1, 2, CellContent.WHITE);
+			board.addPiece(2, 2, CellContent.WHITE);
+			board.addPiece(2, 1, CellContent.WHITE);
+			board.addPiece(2, 0, CellContent.WHITE);
+			board.addPiece(1, 0, CellContent.WHITE);
+			board.addPiece(1, 1, CellContent.BLACK);
+
 			board.updateScore();
 			assertEquals((Integer) 1,(Integer) board.getWhiteScore() );
 			assertEquals((Integer) 0,(Integer) board.getBlackScore() );
@@ -59,12 +64,13 @@ public class BoardTest {
 	//  - w -
 	public void add_loop_diamond(){
 		board = new Board(7);
-		board.addPiece(2, 0, CellContent.WHITE);
-		board.addPiece(0, 1, CellContent.WHITE);
-		board.addPiece(1, 2, CellContent.WHITE);
-		board.addPiece(2, 1, CellContent.WHITE);
-		board.addPiece(1, 1, CellContent.BLACK);
 		try {
+			board.addPiece(2, 0, CellContent.WHITE);
+			board.addPiece(0, 1, CellContent.WHITE);
+			board.addPiece(1, 2, CellContent.WHITE);
+			board.addPiece(2, 1, CellContent.WHITE);
+			board.addPiece(1, 1, CellContent.BLACK);
+
 			board.updateScore();
 			assertEquals((Integer) 1,(Integer) board.getWhiteScore() );
 			assertEquals((Integer) 0,(Integer) board.getBlackScore() );
@@ -79,24 +85,25 @@ public class BoardTest {
 	//  - w w w w
 	public void add_loop_doubly(){
 		board = new Board(7);
-		board.addPiece(2, 0, CellContent.WHITE);
-		board.addPiece(0, 1, CellContent.WHITE);
-		board.addPiece(1, 2, CellContent.WHITE);
-		board.addPiece(2, 1, CellContent.WHITE);
-		
-		board.addPiece(1, 1, CellContent.BLACK);
-		
-		board.addPiece(2, 2, CellContent.WHITE);
-		board.addPiece(3, 0, CellContent.WHITE);
-		board.addPiece(3, 2, CellContent.WHITE);
-		board.addPiece(4, 1, CellContent.WHITE);
-		board.addPiece(4, 2, CellContent.WHITE);		
-		
-		board.addPiece(3, 1, CellContent.BLACK);
-		
-		
-		
 		try {
+			board.addPiece(2, 0, CellContent.WHITE);
+			board.addPiece(0, 1, CellContent.WHITE);
+			board.addPiece(1, 2, CellContent.WHITE);
+			board.addPiece(2, 1, CellContent.WHITE);
+			
+			board.addPiece(1, 1, CellContent.BLACK);
+			
+			board.addPiece(2, 2, CellContent.WHITE);
+			board.addPiece(3, 0, CellContent.WHITE);
+			board.addPiece(3, 2, CellContent.WHITE);
+			board.addPiece(4, 1, CellContent.WHITE);
+			board.addPiece(4, 2, CellContent.WHITE);		
+			
+			board.addPiece(3, 1, CellContent.BLACK);
+			
+			
+		
+
 			board.updateScore();
 			assertEquals((Integer) 2,(Integer) board.getWhiteScore() );
 			assertEquals((Integer) 0,(Integer) board.getBlackScore() );
@@ -116,26 +123,27 @@ public class BoardTest {
 	public void add_big_loop(){
 		add_loop_diamond();
 		// left side
-		board.addPiece(1, 3, CellContent.WHITE);
-		board.addPiece(1, 4, CellContent.WHITE);
-		board.addPiece(1, 5, CellContent.WHITE);
-
-		
-		// bottom side
-		board.addPiece(2, 5, CellContent.WHITE);
-		board.addPiece(3, 5, CellContent.WHITE);
-		board.addPiece(4, 5, CellContent.WHITE);
-		 
-		// right side
-		board.addPiece(4, 4, CellContent.WHITE);
-		board.addPiece(4, 3, CellContent.WHITE);
-		board.addPiece(4, 2, CellContent.WHITE);
-		
-		// top side
-		board.addPiece(3, 2, CellContent.WHITE);
-		board.addPiece(2, 2, CellContent.WHITE);
-		
 		try {
+			board.addPiece(1, 3, CellContent.WHITE);
+			board.addPiece(1, 4, CellContent.WHITE);
+			board.addPiece(1, 5, CellContent.WHITE);
+	
+			
+			// bottom side
+			board.addPiece(2, 5, CellContent.WHITE);
+			board.addPiece(3, 5, CellContent.WHITE);
+			board.addPiece(4, 5, CellContent.WHITE);
+			 
+			// right side
+			board.addPiece(4, 4, CellContent.WHITE);
+			board.addPiece(4, 3, CellContent.WHITE);
+			board.addPiece(4, 2, CellContent.WHITE);
+			
+			// top side
+			board.addPiece(3, 2, CellContent.WHITE);
+			board.addPiece(2, 2, CellContent.WHITE);
+			
+
 			board.updateScore();
 			assertEquals((Integer) 5,(Integer) board.getWhiteScore() );
 			assertEquals((Integer) 0,(Integer) board.getBlackScore() );
